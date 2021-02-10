43bed363da42e7135695d6aa4f7bd8b3c143ebe7
Marcos Leonardo Martins <mleonardomartins@gmail.com>
2021-02-10 00:01:50 -0300

    Aula_03 PT 1

---
+++ scripts.js
@@ -0,0 +1,81 @@
+/* Objeto com duas funções, como se fossem properties. São chamadas métodos */
+const Modal = {
+    open() {
+        document
+            .querySelector(".modal-overlay")
+            .classList
+            .add("active");
+    },
+    close() {               
+        document
+            .querySelector(".modal-overlay")
+            .classList
+            .remove("active");
+    }
+} 
+
+const transactions = [
+    {
+        id: 1,
+        description: "Luz",
+        amount: -50000,
+        date: '23/01/2021'
+    },
+    {
+        id: 2,
+        description: "WebSite",
+        amount: 500000,
+        date: '27/01/2021'        
+    },
+    {
+        id: 3,
+        description: "Internet",
+        amount: -20000,
+        date: '10/02/2021'
+    }
+]
+
+const Transaction = {
+    incomes() {
+        // Somar as Entradas
+    },
+
+    expenses() {
+        // Somar as saídas
+    },
+
+    totals() {
+        //Entradas - Saídas
+    }
+}
+
+// Trabalha-se muito com objetos e nele podem conter properties e metodos, separados por virgulas.
+const DOM = {
+    containerTransaction: document.querySelector("#data-table tbody"),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement("tr");
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction); 
+        
+        DOM.containerTransaction.appendChild(tr);
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"; //if reduzido
+
+        const html = `       
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover Transação">
+            </td>`;
+
+        return html;
+    }
+}
+
+//DOM.addTransaction(transactions[0]);
+
+transactions.forEach( function(transaction) {
+    DOM.addTransaction(transaction);
+}) //forEach vai percorrer cada elemento do array
